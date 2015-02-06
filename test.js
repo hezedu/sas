fc9466b7b852eb6580bcea357a1fdde071412bd4
hezedu@hotmail.com <hezedu@hotmail.com>
2015-02-06 18:37:13 +0800

    dd

--- test.js
+++ test.js
@@ -9,17 +9,19 @@ function v4(a){
 
     var ty = is(a);
     var i = 0;
+    var Max=0;
 
     switch(ty){
       case 'Array':
-
       _for_arr(i,a);
       break;
 
       case 'Object':
+      _for_obj(a);
       break;
 
       case 'function':
+      a(_cb);
       break;
 
       default:
@@ -27,26 +29,22 @@ function v4(a){
     }
 
     function _nexttick(re){
-
       cbre=re;
-
       _for_arr(i+1,a);
+    }
 
+    function _cb(err,re){
+      
+      _nexttick(re);
     }
 
     function _for_arr(i, a) {
       if (i < Max) {
 
-        _dp(a[i],a[i]);
+        _dp(a[i]);
 
       } 
     }
-
-
-
-
-
-
   }
 
 }
