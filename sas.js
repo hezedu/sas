518fda322d60b9c7752f12b67371f1d3ce84514e
hezedu <hezedu@hotmail.com>
2015-02-24 20:19:04 +0800

    format

--- sas.js
+++ sas.js
@@ -1,27 +1,27 @@
 function sas(arr, opt) {
   opt = opt || {};
   var debug = opt.debug || sas.debug,
-  C_stop=false;
+    C_stop = false;
   //DEBUG 1 共五处 
   if (debug) {
     _color(1, '开始', 22);
     var C_START = Date.now(),
       C_time = 0;
-      
-  function _color(c, str, b) {
-        b = b || 39;
-        if (typeof window !== 'undefined') {
-          console.log(str);
-        } else {
-          console.log('\u001b[' + c + 'm' + str + '\u001b[' + b + 'm');
-        }
+
+    function _color(c, str, b) {
+      b = b || 39;
+      if (typeof window !== 'undefined') {
+        console.log(str);
+      } else {
+        console.log('\u001b[' + c + 'm' + str + '\u001b[' + b + 'm');
       }
     }
+  }
   var C_count = [arr.length, 0];
   _dis(C_count[1], arr, C_count);
 
   function _dis(i, t, count, parents) {
-    if(C_stop){
+    if (C_stop) {
       return;
     }
     var ty = Object.prototype.toString.call(t[i]).slice(8, -1);
@@ -43,15 +43,15 @@ function sas(arr, opt) {
             path: [i]
           },
           j = 0,
-          ps,isSP=false;
+          ps, isSP = false;
         if (parents) {
           ps = parents;
           //ext.parent = parents[1];
           while (ps) {
             j++;
-            if(!isSP && typeof ps[0]==='number'){
-              ext.Sparent=ps[1];
-              isSP=true;
+            if (!isSP && typeof ps[0] === 'number') {
+              ext.Sparent = ps[1];
+              isSP = true;
             }
             ext.path.splice(0, 0, ps[0]);
             ps = ps[3];
@@ -72,14 +72,15 @@ function sas(arr, opt) {
           var _start = Date.now();
           var path = ext.path.join('/');
           var a_or_sa_c = 90,
-              a_or_sa_str = 'AS';
-            if (typeof i === 'number') {
-              a_or_sa_c = 37;
-              a_or_sa_str = 'S ';
-            }
+            a_or_sa_str = 'AS';
+          if (typeof i === 'number') {
+            a_or_sa_c = 37;
+            a_or_sa_str = 'S ';
+          }
         }
         var args = arguments;
         t[i](_cb, ext);
+
         function _next_tick(i, t, count, parents) {
           if (count[0] === count[1]) {
             if (parents) {
@@ -90,8 +91,8 @@ function sas(arr, opt) {
               _color(96, '统计：' + C_time + 'ms');
               var time2 = Date.now() - C_START;
               _color(96, '实计用时：' + time2 + 'ms');
-              time2=C_time - time2;
-              _color(36, '节省：' + (time2>=0 ? time2 : '--') + 'ms');
+              time2 = C_time - time2;
+              _color(36, '节省：' + (time2 >= 0 ? time2 : '--') + 'ms');
             }
           } else {
             if (typeof i === 'number') {
@@ -99,23 +100,24 @@ function sas(arr, opt) {
             }
           }
         }
+
         function _cb(result) {
-       if(C_stop){
-        return;
-      }
-          if(result==='$STOP'){
-            //DEBUG 4
-           debug && _color(91, path + '\t' + a_or_sa_str + ':' + result);
-            return C_stop=true;
+          if (C_stop) {
+            return;
           }
-          if(arguments.length<2){
+          if (result === '$STOP') {
+            //DEBUG 4
+            debug && _color(91, path + '\t' + a_or_sa_str + ':' + result);
+            return C_stop = true;
+          }
+          if (arguments.length < 2) {
             t[i] = result;
-          }else{
-            var result_tmp=[];
-            for(var r_i=0,len=arguments.length;r_i<len;r_i++){
+          } else {
+            var result_tmp = [];
+            for (var r_i = 0, len = arguments.length; r_i < len; r_i++) {
               result_tmp.push(arguments[r_i]);
             }
-            t[i]= result_tmp;
+            t[i] = result_tmp;
           }
           count[1] ++;
           //DEBUG 5
@@ -123,7 +125,7 @@ function sas(arr, opt) {
             var time = Date.now() - _start;
             C_time += time;
 
-            _color(a_or_sa_c,a_or_sa_str + ':[' + count[0] + '/' + count[1] + ']\t' +path + '\t' +  time + 'ms');
+            _color(a_or_sa_c, a_or_sa_str + ':[' + count[0] + '/' + count[1] + ']\t' + path + '\t' + time + 'ms');
           }
           _next_tick.apply(null, args);
         }
@@ -138,4 +140,6 @@ function sas(arr, opt) {
     }
   }
 }
-module.exports = sas;
+if (typeof module === 'object' && typeof module.exports === 'object') {
+  module.exports = sas;
+}
