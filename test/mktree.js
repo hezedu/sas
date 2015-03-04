de7b0ef5ac301c7b300df6ef323a3cdbc887d6ab
hezedu@hotmail.com <hezedu@hotmail.com>
2015-03-04 18:14:15 +0800

    http 删掉

--- test/mktree.js
+++ test/mktree.js
@@ -2,57 +2,57 @@ var sas = require('../sas');
 var fs = require('fs');
 
 var mktree = function(path) {
-	return function(cb, ext) {
-		if (ext.Sparent) {
-			path = ext.Sparent[0] + path;
-		}
-		fs.mkdir(path, 777, function(err, result) {
-			if (err) {
-				return cb('$STOP');
-			}
-			cb(path);
-		});
-	}
+  return function(cb, ext) {
+    if (ext.Sparent) {
+      path = ext.Sparent[0] + path;
+    }
+    fs.mkdir(path, 777, function(err, result) {
+      if (err) {
+        return cb('$STOP');
+      }
+      cb(path);
+    });
+  }
 }
 var plan = [
-	mktree(__dirname + '/root' + Date.now()), {
-		'1': [mktree('/1'), {
-			'1-1': mktree('/1-1'),
-			'1-2': mktree('/1-2'),
-			'1-3': mktree('/1-3')
-		}],
-		'2': [mktree('/2'), {
-			'2-1': mktree('/2-1'),
-			'2-2': mktree('/2-2'),
-			'2-3': mktree('/2-3')
-		}],
-		'3': [mktree('/3'), {
-			'3-1': mktree('/3-1'),
-			'3-2': mktree('/3-2'),
-			'3-3': mktree('/3-3')
-		}]
-	}
+  mktree(__dirname + '/root' + Date.now()), {
+    '1': [mktree('/1'), {
+      '1-1': mktree('/1-1'),
+      '1-2': mktree('/1-2'),
+      '1-3': mktree('/1-3')
+    }],
+    '2': [mktree('/2'), {
+      '2-1': mktree('/2-1'),
+      '2-2': mktree('/2-2'),
+      '2-3': mktree('/2-3')
+    }],
+    '3': [mktree('/3'), {
+      '3-1': mktree('/3-1'),
+      '3-2': mktree('/3-2'),
+      '3-3': mktree('/3-3')
+    }]
+  }
 ];
 
 var plan2 = [
-	__dirname + '/root' + Date.now(), {
-		'1': ['/1', {
-			'1': '/1-1',
-			'1-2': '/1-2',
-			'1-3': '/1-3'
-		}],
-		'2': ['/2', {
-			'2-1': '/2-1',
-			'2-2': '/2-2',
-			'2-3': '/2-3'
-		}],
-		'3': ['/3', {
-			'3-1': '/3-1',
-			'3-2': '/3-2',
-			'3-3': '/3-3'
-		}]
-	}
+  __dirname + '/root' + Date.now(), {
+    '1': ['/1', {
+      '1': '/1-1',
+      '1-2': '/1-2',
+      '1-3': '/1-3'
+    }],
+    '2': ['/2', {
+      '2-1': '/2-1',
+      '2-2': '/2-2',
+      '2-3': '/2-3'
+    }],
+    '3': ['/3', {
+      '3-1': '/3-1',
+      '3-2': '/3-2',
+      '3-3': '/3-3'
+    }]
+  }
 ];
 sas(plan, {
-	debug: true,iterator:mktree
+  debug: true,iterator:mktree
 });
