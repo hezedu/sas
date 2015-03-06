a79201ef7046f56db0242f838419b920733f91c2
hezedu@hotmail.com <hezedu@hotmail.com>
2015-03-06 16:59:49 +0800

    002

--- test/search.js
+++ test/search.js
@@ -8,11 +8,19 @@ var sas = require('../sas');
 
 //sas.debug = true;
 
-var result = [],
-  find_name = 'sas.js',
-  from = 'D:/WWW/rockmongo';
 
-function _fspath(arr) {
+
+
+var find_name = 'mktree.js', //寻找的目标
+  from = '/';  //从哪个目录开始
+
+var result = [];  //寻找结果
+  var file_c1=0;  //文件统计
+  var files_c2=0; //文件夹统计
+
+
+
+function _fspath(arr,dir) {
   var strtmp = '',
     arr_len = arr.length;
   for (var i = 0; i < arr_len; i++) {
@@ -20,35 +28,34 @@ function _fspath(arr) {
       strtmp += arr[i];
     }
   }
-  return strtmp;
+  return dir+'/'+strtmp;
 }
 
 
 function read_dir(path) {
   return function(cb, t) {
 
-    var fspath =_fspath(t.path);
-   //console.log('\n\nstrtmp' +fspath)
-/*    if (t.Sparent) {
-      var ts0 = t.Sparent[0];
-      ts0 = ts0 === '/' ? '' : ts0;
-      path = ts0 + path;
-    }*/
-    //var _path = (path === '/') ? path : path + '/';
-    fspath= fspath || '/';
+    var fspath =_fspath(t.path,from);
+    //console.log("fspath=" + fspath);
+    //fspath= fspath || '/';
     fs.readdir(fspath, function(err, files) {
-      if (err) {
-        console.log('read_dir Err= ' + err);
+      if (err) {  //一些奇怪的文件夹
+        //console.log('read_dir Err= ' + err);
         return cb('$end');
       }
       var obj = {};
+      var len = files.length;
+      if(!len){
+        return cb('$end');//空文件夹
+      }
 
-      for (var i = 0, len = files.length; i < len; i++) {
+      for (var i = 0; i < len; i++) {
         var file = files[i];
+        
         if (file === find_name) {
-          result.push[fspath +'/'+ file];
+          result.push(fspath +'/'+ file); //保存搜索到的结果
         }
-        obj['/' + file] = fspath +'/'+ file;
+        obj['/' + file] = fspath +'/'+ file; //防止跟保留字冲突，前面加 '/';
       }
 
       t.push(obj);
@@ -60,35 +67,45 @@ function read_dir(path) {
 function _stat(path) {
   return function(cb, t) {
     fs.stat(path, function(err, stat) {
-      if (err) {
-        console.log('read_dir Err= ' + err);
-        return cb('$end');
+      if (err) {//一些奇怪的文件
+        //console.log('read_dir Err= ' + err);
+        return cb('$end');  
       }
-      console.log('path=='+path);
-      try {
+
+      //try {  //一些
         if (stat.isDirectory()) {
-          /*          t.reload([read_dir(path)]); //替换当前为数组,并重载当前任务。
-
-                    return cb('$RELOAD');*/
-          //t.reload([read_dir(path)]); //替换当前为数组,并重载当前任务。
-
+          files_c2++;
           return cb('$RELOAD', [read_dir(path)]);
+        }else{
+          file_c1++;
         }
-      } catch (e) {
+/*      } catch (e) {
         console.log('catch err =' + e.stack);
 
-      }
+      }*/
       cb(path);
 
     });
   }
 }
 
-//read_dir(from)(function(){},{})
 var plan = [read_dir(from)];
+
+console.time('用时');
+
 sas(plan, {
   iterator: _stat,
   allEnd:function(){
-    console.log('\n '+result);
+    console.log('\n=======================');
+    console.timeEnd('用时');
+    console.log('(包括隐藏文件夹)');
+    console.log('文件夹： '+files_c2+'个');
+    console.log('文件： '+file_c1+'个');
+    console.log('共： '+(file_c1+files_c2)+'个');
+    console.log('寻找结果：'+result.length+'个\n'+result.join('\n'));
+    console.log('=======================\n');
   }
-})
+});
+/*
+window 对应命令行如：  dir d:\ /a /s | find "mktree.js"
+*/
