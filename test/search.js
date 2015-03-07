68a3a0e1348c85254c9c489c81c2c0dc3ef4c08f
hezedu <hezedu@hotmail.com>
2015-03-07 17:18:34 +0800

    瞬间爆炸

--- test/search.js
+++ test/search.js
@@ -12,17 +12,14 @@ sas.debug = true; */
 
 
 
-
-
-
-
 var find_name = 'sas.js', //顺带搜索的目标
   from = '/'; //从哪个目录开始
-  //from = 'D:/git/sas'; 
+//from = 'D:/git/sas'; 
 
 var result = []; //搜索结果
 var file_c1 = 0; //文件统计
 var files_c2 = 0; //文件夹统计
+var index2 = {}; //索引存放处
 
 
 
@@ -62,6 +59,7 @@ function read_dir(cb, t) {
         result.push(fspath + '/' + file); //保存搜索到的结果
       }
       obj['/' + file] = fspath + '/' + file; //防止跟保留字冲突，前面加 '/';
+      index2[fspath + '/' + file] = file;
     }
 
     t.push(obj);
@@ -75,8 +73,8 @@ function _stat(path) {
       if (err) { //一些奇怪的文件
         //console.log('_stat Err= ' + err);
         return cb();
-      } 
-      if(stat.isSymbolicLink()){//linux坑 软链接
+      }
+      if (stat.isSymbolicLink()) { //linux坑 软链接
         return cb();
       }
       //try {  //一些
@@ -90,7 +88,7 @@ function _stat(path) {
               console.log('catch err =' + e.stack);
 
             }*/
-      cb('lstat');
+      cb();
 
     });
   }
@@ -106,7 +104,7 @@ console.time('\u001b[91m用时\u001b[39m');
 sas(plan, { //////核心
   iterator: _stat,
   allEnd: function() {
-    
+
     console.timeEnd('\u001b[91m用时\u001b[39m');
     console.log('\n文件夹： \u001b[96m' + files_c2 + '\u001b[39m个');
     console.log('文件： \u001b[96m' + file_c1 + '\u001b[39m个');
@@ -116,11 +114,11 @@ sas(plan, { //////核心
     console.log('=======================\n');
     console.log('\n\u001b[93m' + from + '\u001b[39m \u001b[91m目录下所有文件索引已建立!\u001b[39m\n');
     console.log('再次搜索看看是不是变快了？ ');
-    research();
+    //research();
+    research2();
 
-/*        
-        //输出到文件
-        fs.writeFile('./index.json', JSON.stringify(plan), null, function(err) {
+    //输出到文件
+    /*    fs.writeFile('./index.json', JSON.stringify(index2), null, function(err) {
           if (err) {
             console.log(err);
           } else {
@@ -132,82 +130,38 @@ sas(plan, { //////核心
   }
 });
 
-
-
-function research() {
-
+function research2() {//在建立索引的情况下搜索
 
   process.stdin.read();
 
   process.stdin.on('data', function(chunk) {
-
-    var search_name = '/' + chunk.toString(),
+    var search_name = chunk.toString(),
       result2 = [];
-      var last_len = process.platform ==='linux' ? 1 : 2; //window 下换行为2 linux为1 
-    search_name = search_name.substr(0, search_name.length - last_len); 
-    console.log("process.platform===" + process.platform);
+    var last_len = process.platform === 'linux' ? 1 : 2; //window 下换行为2 linux为1 
+    search_name = search_name.substr(0, search_name.length - last_len);
+    console.log('search_name==' + search_name)
 
-    console.log('在 \u001b[93m' + from + '\u001b[39m 目录下开始查找所有的 \u001b[91m' + search_name.substr(1) + '\u001b[39m');
+    console.log('在 \u001b[93m' + from + '\u001b[39m 目录下开始查找所有的 \u001b[91m' + search_name + '\u001b[39m');
 
     console.log('正在查找，请稍等……');
 
     console.time('\u001b[91m建索引后用时\u001b[39m');
-    sas(plan, {
-      iterator: _search,
-      allEnd: function() {
-        console.timeEnd('\u001b[91m建索引后用时\u001b[39m');
-        if (!result2.length) {
-          console.log('搜索结果为：空');
-        } else {
-          console.log('\n=======================');
-          console.log('搜索结果：' + result2.length + '个\n' + result2.join('\n'));
-          console.log('=======================\n');
-        }
+    //console.log(JSON.stringify(index2));
 
-        console.log('\n继续搜索:');
-        process.stdin.resume();
-
-        //research();
-      }
-    });
-
-    function _search(str) {
-      return function(cb, t) {
-        var filename = t.index;
-        if (!str) { //目录
-          filename = t.pIndex;
-        }
-        //console.log('filename=='+filename)
-        if (filename === search_name) {
-          result2.push(_fspath(t.path, from));
-        }
-        cb(str);
+    for (var i in index2) {
+      if (index2[i] === search_name) {
+        result2.push(i);
       }
     }
-
+    console.timeEnd('\u001b[91m建索引后用时\u001b[39m');
+    if (!result2.length) {
+      console.log('搜索结果为：空');
+    } else {
+      console.log('\n=======================');
+      console.log('搜索结果：' + result2.length + '个\n' + result2.join('\n'));
+      console.log('=======================\n');
+    }
+    console.log('\n继续搜索:');
+    process.stdin.resume();
   });
-
-
-
 }
-
-
-//'\u001b[' + c + 'm' + str + '\u001b[' + b + 'm'
-
-/*
-*简单的测试了下，我这边的情况是这样的：
-
-*测试环境：老XP 32位。双核 2G内存  D：盘。由本程序统计共： 121435个文件。
-*window 对应命令行如：  dir d:\ /a /s | find "sas.js"
-
-*测试结果，当前在没有索引的情况下，9782ms , 比系统慢， 有索引的情况 1671ms ;下差不多一样快。
-*当然win系统还匹配名字里带搜索字的，而我只简单匹配的是全名。
-*因此不是很准。
-
-*测试环境2：linux 64位。单核。1G内存  根目录  由本程序统计共： 164874个文件。
-*linux下也测试通过了。没建索引 11403ms 建了之后 2441ms 情况跟win 差不多。
-*但对linux文件系统不太熟，只用了命令 find / -name "sas.js" 做的对比。 比我的快的多了。
-*
-*当然sas.js 可以优化的地方还有很多，比如那个索引感觉就是渣。
-*
-*/
